91da4d6a54b11797e1b1521f7595a08f497e7d44
Chethan Kumar <chethankuma@adobe.com>
2023-01-03 18:15:18 +0530

    Refactor

--- index.js
+++ index.js
@@ -16,40 +16,39 @@
 // });
 
 $(function () {
-    $(document).scroll(function () {
-        var $nav = $(".sticky-top");
-        var $banner = $(".hero-banner")
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $banner.height());
-      });
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    var $banner = $(".hero-banner");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $banner.height());
   });
+});
 
+$(".owl-carousel").owlCarousel({
+  autoplay: true,
+  autoplayTimeout: 8000,
+  autoplayHoverPause: true,
+  loop: true,
+  margin: 50,
+  responsiveClass: true,
+  nav: true,
+  loop: true,
+  stagePadding: 100,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    568: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
 
-
-$('.owl-carousel').owlCarousel({
-    autoplay: true,
-    autoplayTimeout: 8000,
-    autoplayHoverPause: true,
-    loop: true,
-    margin: 50,
-    responsiveClass: true,
-    nav: true,
-    loop: true,
-    stagePadding: 100,
-    responsive: {
-        0: {
-            items: 1
-        },
-        568: {
-            items: 2
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 3
-        }
-    }
-})
 $(document).ready(function () {
   $(".popup-youtube").magnificPopup({
     disableOn: 320,
@@ -64,7 +63,7 @@ $(".item").magnificPopup({
   delegate: "a",
 });
 
-document
+/* document
   .querySelector(
     "body > main > section > article:nth-child(1) > div > nav > button "
   )
@@ -82,9 +81,18 @@ document
         .querySelector(".navbar.navbar-dark.navbar-expand-lg")
         .classList.toggle("darkBg");
     }
-  });
+  }); */
 
-if (window.innerWidth < 768) {
+/* if (window.innerWidth < 768) {
   document.querySelector(".cmp-details__desc1").innerHTML =
     '<div class="cmp-details__desc1"><div class="content-desc" style="width:85%; margin: auto"><p class="para-text red-bg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere orci ut felis dictum, a lacinia mauris mattis. Integer ut nibh vitae tortor lobortis elementum. Etiam vehicula, arcu et faucibus tristique, est sem efficitur ligula, at porta quam felis ac ipsum. Aenean eu auctor velit, et mattis urna. Maecenas vulputate tempor dui, convallis facilisis elit accumsan et. Donec mattis, elit eget tempus porta, dui magna aliquet est, sit amet gravida purus diam eget arcu. Maecenas velit tortor, sagittis sed mi ac, dapibus sollicitudin nibh. Curabitur maximus convallis metus, eget euismod ante ornare vel. Duis molestie mauris ex, eget dignissim nisi ullamcorper sodales. Proin imperdiet eleifend mattis.</p></div></div>';
-}
+} */
+
+$(document).click(function (event) {
+  var clickover = $(event.target);
+  var $navbar = $(".navbar-collapse");
+  var _opened = $navbar.hasClass("show");
+  if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+    $navbar.collapse("hide");
+  }
+});
